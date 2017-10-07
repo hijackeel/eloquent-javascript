f0d45e5772b445d06c5bdfe8fcf604a13c03e9ef
Hijack Eel <hjkl@semicolonpipe.io>
2017-10-07 00:13:59 -0700

    Add min.js

---
+++ 03-functions/min.js
@@ -0,0 +1,17 @@
+// Write a function "min" that takes two arguments and returns their minimum.
+
+function min(a, b)
+{
+  return a < b ? a : b;
+}
+
+for (a = -5; a <= 5; a++)
+{
+  for (b = 5; b >= -5; b--)
+  {
+    console.log("a = " + a +
+              "\nb = " + b +
+            "\nmin = " + min(a,b) +
+            "\n\n");
+  }
+}
