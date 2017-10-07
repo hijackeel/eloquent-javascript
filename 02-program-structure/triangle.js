6d4a2c544d5f72c4b9840228a1b2e90676dec7d2
Hijack Eel <hjkl@semicolonpipe.io>
2017-10-06 21:36:52 -0700

    Add triangle.js

---
+++ 02-program-structure/triangle.js
@@ -0,0 +1,14 @@
+/* Write a loop that makes seven calls to console.log to output this triangle:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+for (let step = '#'; step.length <= 7; step += '#')
+{
+  console.log(step);
+}
